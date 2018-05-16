c6789cfdf994f63f678ab36a7f7ccacbc0d5fbf5
mrnagydavid <mrnagydavid@gmail.com>
2018-05-16 14:47:28 +0200

    works for British English Numerals, not for American (which is expected)

--- js-numerals/src/js-numeral.js
+++ js-numerals/src/js-numeral.js
@@ -1,1 +1,58 @@
 // mrnagydavid
+function convertNumeralToText(numeral) {
+  const ones        = ['zero',    'one',    'two',    'three',    'four',     'five',     'six',      'seven',      'eight',    'nine'];
+  const teens       = ['ten',     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',  'sixteen',  'seventeen',  'eighteen', 'nineteen'];
+  const tens        = [undefined, 'ten',    'twenty', 'thirty',   'forty',   'fifty',    'sixty',    'seventy',    'eighty',   'ninety'];
+  const orders      = [10**12,     10**9,     10**6,     10**3,      10**2];
+  const orderValue  = ['trillion', 'billion', 'million', 'thousand', 'hundred'];
+
+  function parseInt(number, orderIndex = -1) {
+    const result = [];
+    let div, mod;
+
+    /* 99 -> infinity */
+    if (number > 99) {
+      const magnitudeIndex = orders.findIndex((order, ind) => {
+        if (ind <= orderIndex) return false;    // we start 'testing' only the orders of magnitude from the intended index
+        div = Math.floor(number / order);
+        return div > 0;                         // div > 0 -> we found the greatest order of magnitude that fits for the numeric value
+      });
+
+      mod = number % orders[magnitudeIndex];
+      const magnitude = orderValue[magnitudeIndex];
+      result.push(parseInt(div, magnitudeIndex));
+      result.push(magnitude);
+      if (mod > 0) {
+        if (mod < 100) {
+          result.push('and');                 // exceptional: hundreds are connected to teens and ones with 'and'
+        }
+        result.push(parseInt(mod));           // only parse the right tree if there is something (mod > 0), otherwise we'd end up with 'zero'
+      }
+
+      return result.join(' ');
+
+    /* 0 -> 99 */
+    } else {
+      /* 0 -> 9 */
+      if (number < 10) {
+        return ones[number]; 
+      }
+      /* 10 -> 19 */
+      if (number > 9 && number < 20) {
+        return teens[number - 10];
+      }
+      /* 20 -> 99 */
+      if (number >= 20) {                   // exceptional: 20 - 99 numbers are hyphenated e.g. twenty-one
+        div = Math.floor(number / 10);
+        mod = number - div * 10;
+        result.push(tens[div]);
+        if (mod > 0) {
+          result.push(ones[mod]);
+        }
+        return result.join('-');
+      }
+    }
+  }
+
+  return parseInt(numeral);
+}
--- js-numerals/test/tests.js
+++ js-numerals/test/tests.js
@@ -1,3 +1,68 @@
-QUnit.test("test init", (assert) => {
-  assert.ok(true);
+QUnit.module('test framework setup');
+
+QUnit.test('function is accessible', (assert) => {
+  assert.ok(convertNumeralToText);
+});
+
+/* -- */
+
+function dictTest(arr, assert) {
+  for (let i = 0; i < arr.length; i = i + 2) {
+    assert.deepEqual(convertNumeralToText(arr[i]), arr[i+1], `${arr[i]} is ${arr[i]}`);
+  }
+}
+
+QUnit.module('js-numeral');
+
+QUnit.test('ones', (assert) => {
+  const test = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  test.forEach((val, ind) => {
+    assert.deepEqual(convertNumeralToText(ind), val, `${ind} is ${val}`);
+  })
+});
+
+QUnit.test('teens', (assert) => {
+  const test = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  test.forEach((val, ind) => {
+    assert.deepEqual(convertNumeralToText(ind + 10), val, `${ind+10} is ${val}`);
+  })
+});
+
+QUnit.test('tens', (assert) => {
+  const test = [10, 'ten', 20, 'twenty', 30, 'thirty', 40, 'forty', 50, 'fifty', 60, 'sixty', 70, 'seventy', 80, 'eighty', 90, 'ninety'];
+  dictTest(test, assert);
+});
+
+QUnit.test('20-99', (assert) => {
+  const test = [20, 'twenty', 21, 'twenty-one', 32, 'thirty-two', 43, 'forty-three', 54, 'fifty-four', 65, 'sixty-five', 76, 'seventy-six', 87, 'eighty-seven', 98, 'ninety-eight'];
+  dictTest(test, assert);
+});
+
+QUnit.test('10^n', (assert) => {
+  const test = [
+    10**2, 'one hundred',
+    10**3, 'one thousand',
+    10**4, 'ten thousand',
+    10**5, 'one hundred thousand',
+    10**6, 'one million',
+    10**7, 'ten million',
+    10**8, 'one hundred million',
+    10**9, 'one billion',
+    10**10, 'ten billion',
+    10**11, 'one hundred billion',
+    10**12, 'one trillion',
+    10**16, 'ten thousand trillion'
+  ];
+  dictTest(test, assert);
+});
+
+QUnit.test('DiNa test cases', (assert) => {
+  const test = [
+    7,      'seven',
+    42,     'forty-two',
+    2001,   'two thousand and one',
+    1999,   'nineteen hundred and ninety-nine',
+    17999,  'seventeen thousand nine hundred and ninety-nine'
+  ];
+  dictTest(test, assert);
 });
