40a10dd6cecf14d9896ae5882730992663f68436
David Nagy <mrnagydavid@gmail.com>
2018-05-25 10:05:08 +0200

    initial scaffolding

---
+++ js-users/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = axios;
+
+new Vue({
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app');
+
